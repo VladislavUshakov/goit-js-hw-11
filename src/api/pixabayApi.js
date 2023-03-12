62039204d81607471ed7275151e5f6ae40929230
Vladyslav Ushakov <56474123+VladislavUshakov@users.noreply.github.com>
2023-03-12 22:39:20 +0200

    Add base functional

--- src/api/pixabayApi.js
+++ src/api/pixabayApi.js
@@ -1,22 +1,24 @@
 const axios = require('axios/dist/browser/axios.cjs');
 
+const params = {
+  q: '',
+  key: '34239282-bbeea62304f42d8ced9502c1f',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: 1,
+  per_page: 40,
+};
+
 export class PixabayApi {
   constructor() {
-    this.params = {
-      q: '',
-      key: '34239282-bbeea62304f42d8ced9502c1f',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: 1,
-      per_page: 40,
-    };
+    this.params = params;
+    this.responseItems = [];
   }
 
-  // For tests
-  //   set query(text) {
-  //     this.params.q = text;
-  //   }
+  set query(text) {
+    this.params.q = text;
+  }
 
   get query() {
     return this.params.q;
@@ -26,13 +28,21 @@ export class PixabayApi {
     this.params.page = number;
   }
 
+  get res() {
+    return this.responseItems;
+  }
+
   async getData() {
     try {
       const res = await axios.get(`https://pixabay.com/api/`, {
         params: this.params,
       });
-      return res.data;
+
+      this.params.page += 1;
+      this.responseItems = res.data.hits;
     } catch (error) {
+      this.params.page = 1;
+      this.responseItems = [];
       console.log(error);
     }
   }
--- src/index.js
+++ src/index.js
@@ -8,96 +8,77 @@ const refs = {
 
 const apiService = new PixabayApi();
 
-refs.searchForm.addEventListener('submit', async e => {
+refs.searchForm.addEventListener('submit', searchHandler);
+refs.loadMoreBtn.addEventListener('click', loadMoreHandler);
+
+async function searchHandler(e) {
   e.preventDefault();
 
-  apiService.query = e.target.elements.searchQuery.value;
+  const currentQuery = e.target.elements.searchQuery.value.trim().toLowerCase();
 
-  const res = await apiService.getData();
-  console.log(res);
-});
+  if (!currentQuery) {
+    return;
+  }
 
-// refs.searchForm.addEventListener('submit', searchHandler);
-// refs.loadMoreBtn.addEventListener('click', async e => {
-//   try {
-//     const res = await getData(params);
+  if (currentQuery !== apiService.query) {
+    apiService.query = currentQuery;
+    apiService.page = 1;
+    await apiService.getData();
+    replaceMarkup(apiService.responseItems);
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  }
+}
 
-//     params.page += 1;
-//     addMarkup(res.hits);
-//     refs.loadMoreBtn.classList.remove('is-hidden');
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-//   e.target.blur();
-// });
+async function loadMoreHandler(e) {
+  e.preventDefault();
+  e.target.blur();
 
-// const params = {
-//   q: '',
-//   key: '34239282-bbeea62304f42d8ced9502c1f',
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: true,
-//   page: 1,
-//   per_page: 40,
-// };
+  await apiService.getData();
+  addMarkup(apiService.responseItems);
+}
 
-// async function searchHandler(e) {
-//   e.preventDefault();
+function addMarkup(data) {
+  const markup = renderMarkup(data);
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
 
-//   params.q = e.target.elements.searchQuery.value;
+function replaceMarkup(data) {
+  const markup = renderMarkup(data);
 
-//   try {
-//     const res = await getData(params);
+  refs.gallery.innerHTML = markup;
+}
 
-//     params.page += 1;
-//     addMarkup(res.hits);
-//     refs.loadMoreBtn.classList.remove('is-hidden');
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// }
-
-// async function getData(params) {
-//   const res = await axios.get(`https://pixabay.com/api/`, { params });
-//   return res.data;
-// }
-
-// function addMarkup(data) {
-//   const markup = renderMarkup(data);
-//   refs.gallery.insertAdjacentHTML('beforeend', markup);
-// }
-
-// function renderMarkup(data) {
-//   let markup = '';
-//   data.map(
-//     ({
-//       webformatURL,
-//       largeImageURL,
-//       tags,
-//       likes,
-//       views,
-//       comments,
-//       downloads,
-//     }) => {
-//       markup += `
-//       <div class="photo-card">
-//         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//         <div class="info">
-//           <p class="info-item">
-//             <b>Likes ${likes}</b>
-//           </p>
-//           <p class="info-item">
-//            <b>Views ${views}</b>
-//           </p>
-//           <p class="info-item">
-//             <b>Comments ${comments}</b>
-//           </p>
-//           <p class="info-item">
-//            <b>Downloads ${downloads}</b>
-//           </p>
-//         </div>
-//       </div>`;
-//     }
-//   );
-//   return markup;
-// }
+function renderMarkup(data) {
+  let markup = '';
+  data.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      markup += `
+      <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes ${likes}</b>
+          </p>
+          <p class="info-item">
+           <b>Views ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${comments}</b>
+          </p>
+          <p class="info-item">
+           <b>Downloads ${downloads}</b>
+          </p>
+        </div>
+      </div>`;
+    }
+  );
+  return markup;
+}
